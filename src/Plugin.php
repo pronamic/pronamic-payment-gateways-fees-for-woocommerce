dff86ea656e0d1ee291339d8b9bdbfa8ca25e2a7
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-10-16 16:19:18 +0200

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -90,6 +90,34 @@ class Plugin {
 		);
 
 		\add_action( 'before_woocommerce_pay_form', function( $order, $order_button_text, $available_gateways ) {
+			?>
+			<script type="text/javascript">
+				const url = new URL( window.location );
+
+				jQuery( ( $ ) => {
+					$( document ).ready( function() {
+						$( '#order_review .shop_table' ).wrap( '<div id="pronamic-order"></div>' );
+					} );
+
+					$( 'body' ).on( 'change', 'input[name="payment_method"]', function () {
+						url.searchParams.set( 'pronamic_gateway_fee', this.value );
+
+						$pronamic_order = $( "#pronamic-order" );
+
+						$pronamic_order.css( 'filter', 'blur(5px)' );
+
+						$pronamic_order.load(
+							url + " #order_review .shop_table",
+							function() {
+								$pronamic_order.css( 'filter', 'none' );
+							}
+						);
+					} );
+				} );
+
+			</script>
+			<?php
+
 			if ( ! $order->needs_payment() ) {
 				return;
 			}
@@ -143,34 +171,6 @@ class Plugin {
 			}
 
 			$order->calculate_totals();
-
-			?>
-			<script type="text/javascript">
-				const url = new URL( window.location );
-
-				jQuery( ( $ ) => {
-					$( document ).ready( function() {
-						$( '#order_review .shop_table' ).wrap( '<div id="pronamic-order"></div>' );
-					} );
-
-					$( 'body' ).on( 'change', 'input[name="payment_method"]', function () {
-						url.searchParams.set( 'pronamic_gateway_fee', this.value );
-
-						$pronamic_order = $( "#pronamic-order" );
-
-						$pronamic_order.css( 'filter', 'blur(5px)' );
-
-						$pronamic_order.load(
-							url + " #order_review .shop_table",
-							function() {
-								$pronamic_order.css( 'filter', 'none' );
-							}
-						);
-					} );
-				} );
-
-			</script>
-			<?php
 		}, 10, 3 );
 	}
 
