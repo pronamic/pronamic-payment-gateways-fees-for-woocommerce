516b7796bdd1cd8a6785807a61a9a1140bcbbd0b
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-11-02 13:35:59 +0100

    HPOS compatibility.

--- pronamic-payment-gateways-fees-for-woocommerce.php
+++ pronamic-payment-gateways-fees-for-woocommerce.php
@@ -46,3 +46,18 @@ add_action(
 \Pronamic\WooCommercePaymentGatewaysFees\Plugin::instance()->setup();
 
 \Pronamic\WordPress\Updater\Plugin::instance()->setup();
+
+/**
+ * High Performance Order Storage.
+ * 
+ * @link https://github.com/pronamic/pronamic-payment-gateways-fees-for-woocommerce/issues/4
+ * @link https://github.com/woocommerce/woocommerce/wiki/High-Performance-Order-Storage-Upgrade-Recipe-Book#declaring-extension-incompatibility
+ */
+add_action(
+	'before_woocommerce_init',
+	function () {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
+	}
+);
